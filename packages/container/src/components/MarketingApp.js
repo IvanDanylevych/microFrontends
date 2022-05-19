45739c0da707ba013ac3c9f7cbdaa4fdff53120c
Ivan Ivan <iv.danylevych@gmail.com>
2022-05-19 20:50:40 +0300

    updated router functionality

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,21 @@
 import { mount } from 'marketing/MarketingApp'
+import { useHistory } from 'react-router-dom'
 import React, { useRef, useEffect } from 'react'
 
 export default () => {
+  const history = useHistory()
   const ref = useRef(null)
 
   useEffect(() => {
     if (ref.current) {
-      mount(ref.current)
+      const { onParentNavigate } = mount(ref.current, {
+        onNavigate: ({ pathname: nextPathname }) => {
+          if (history.location.pathname !== nextPathname) {
+            history.push(nextPathname)
+          }
+        },
+      })
+      history.listen(onParentNavigate)
     }
   }, [])
 
